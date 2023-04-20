2aaf2151ef157c6c7ec222c2d718edfb8afcd55e
mark.london <mark.london@gfk.com>
2023-04-20 11:58:10 +0100

    repaired

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import spacy
 from spacy.util import minibatch, compounding
 from spacy.training import Example
 from spacy.training import train_data
-iuiuiuuh
+
+
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
